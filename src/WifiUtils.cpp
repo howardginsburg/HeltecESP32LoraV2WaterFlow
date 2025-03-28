66893140699caa699cc7986607465ec14256d46a
Howard Ginsburg <33068139+howardginsburg@users.noreply.github.com>
2025-03-27 21:57:16 -0400

    increased wifi delay before retry to give it time to connect.

--- src/WifiUtils.cpp
+++ src/WifiUtils.cpp
@@ -1,21 +1,37 @@
 #include <WiFi.h>
 #include "DisplayUtils.h"
 
-//Connects to the wifi network.
+// Connects to the wifi network.
 void initWifi()
 {
-  
-  // If the Wifi Status is not connected or the ip address is 0.0.0.0, then connect to the Wifi.
+  // If the Wifi Status is not connected or the IP address is 0.0.0.0, then connect to the Wifi.
   while ((WiFi.status() != WL_CONNECTED) || (String(WiFi.localIP().toString()) == "0.0.0.0"))
   {
     Serial.printf("Connecting to WIFI SSID %s \n", WIFI_SSID);
-    
-    
     Display.print(0, ("SSID:" + String(WIFI_SSID)).c_str());
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    delay(1000);
+    delay(4000);
+
+    // Check for connection errors
+    if (WiFi.status() != WL_CONNECTED)
+    {
+      switch (WiFi.status())
+      {
+      case WL_NO_SSID_AVAIL:
+        Serial.println("Error: SSID not available.");
+        break;
+      case WL_CONNECT_FAILED:
+        Serial.println("Error: Connection failed. Check password.");
+        break;
+      case WL_DISCONNECTED:
+        Serial.println("Error: Disconnected from WiFi.");
+        break;
+      default:
+        Serial.println("Error: Unknown WiFi connection issue.");
+        break;
+      }
+    }
   }
-  
+
   Display.print(1, "IP:" + String(WiFi.localIP().toString()));
-  
 }
